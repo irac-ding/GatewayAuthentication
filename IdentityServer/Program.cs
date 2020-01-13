6cb04ab9d86ea26342263cb7e3a2c911ca3f2303
irac <iracding@tvunetworks.com>
2020-01-13 13:40:24 +0800

    first commit

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).ConfigureServices(services =>
+            {
+                services.AddMvc();
+
+                services.AddIdentityServer()
+                    .AddDeveloperSigningCredential()
+                    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                    .AddInMemoryApiResources(Config.GetApiResources())
+                    .AddInMemoryClients(Config.GetClients())
+                    .AddTestUsers(Config.GetUsers());
+            }).Configure(app =>
+            {
+                app.UseIdentityServer();
+            });
+        }
+    }
+}
